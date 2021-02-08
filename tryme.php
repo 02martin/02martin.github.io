c85d4e4de744a0199b777782a3026bf6c1d4e3d1
02martin <45698922+02martin@users.noreply.github.com>
2021-02-08 01:08:57 -0600

    Create  tryme.php

---
+++ tryme.php
@@ -0,0 +1,8 @@
+<?php 
+ // Assign the value "Hello!" to the variable "greeting" 
+ $greeting = "Hello!"; 
+ // Assign the value 1 to the variable "month" 
+ $month = 1; 
+ // Assign the value 2021 to the variable "year" 
+ $year = 2021; 
+?>
